005de6204a1d162779c937ca53402524d93b13be
José Manuel <expressmillionaire@hotmail.com>
2018-04-17 00:47:17 +0200

    heroku settings

--- index.js
+++ index.js
@@ -8,17 +8,11 @@ app.get('/', (req, res) => {
 	res.send({ val: 'HELLO' })
 })
 
-
-// const PORT = process.env.PORT || 5000
-//prod vs dev environments
-//env vars - post deployment vars
+const PORT = process.env.PORT || 5000
+// prod vs dev environments
+// env vars - post deployment vars
 
 app.listen(PORT)
-
-
-
-
-
 
 
 
