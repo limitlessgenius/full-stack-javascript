3c368b8450fcc26d72204a1c6848ada61e7f54b5
José Manuel <expressmillionaire@hotmail.com>
2018-04-29 13:49:13 +0200

    renewed keys

--- index.js
+++ index.js
@@ -2,19 +2,31 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-auth20').Strategy
-
+const keys = require('./config/keys')
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(
+	new GoogleStrategy({ 
+		clientID: keys.clientID, 
+		clientSecret: keys.clientSecret, 
+		callBackURL: 'auth/google/callback'
+	}, 
+	accessToken => { 
+
+		console.log('ACCESS TOKEN', accessToken) 
+		
+	})
+)
+
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
-
-//clientID
-// 247670904395-0hvulhlb2rd01batvbcj2qi2l07r1ter.apps.googleusercontent.com
-//clientSecret
-//kSY0Ob8GuBrziO5BE_nkAM7t
-
 
 
 
